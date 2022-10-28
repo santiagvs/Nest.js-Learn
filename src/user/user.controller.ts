9907f3713e91dfb47a3561f292e174700b0d9da3
Santiago <victor.lacsilva0102@gmail.com>
2022-10-28 17:04:04 -0300

    feat: setting up e2e tests and docker test database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator/';
+import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 
 @UseGuards(JwtGuard)
